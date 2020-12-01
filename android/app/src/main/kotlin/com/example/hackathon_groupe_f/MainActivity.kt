5dc6b6b6c28eff397722cbe32abae3c40406b9bc
Allan <allan.vixel@etudiant.univ-rennes1.fr>
2020-12-01 09:28:31 +0100

    ajout json

---
+++ android/app/src/main/kotlin/com/example/hackathon_groupe_f/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hackathon_groupe_f
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- hackathon_groupe_f/android/app/src/main/kotlin/com/example/hackathon_groupe_f/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hackathon_groupe_f
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
